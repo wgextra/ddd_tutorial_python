de49dad0f6f3f071163ff4cd9af544aa3e07ed87
Takumi Kumagai <kumagai.takumi1223@gmail.com>
2020-08-10 14:55:55 +0900

    implemented user

--- circle_app.py
+++ circle_app.py
@@ -1,8 +1,9 @@
+import uuid
+
+
 # value object for circle identifier
 class circleId:
     def __init__(self,value):
-        if value == None:
-            raise ValueError(value)
         self.Value = value
 
 # value object for cicle name
@@ -15,3 +16,13 @@ class circleName:
         self.Value = value
     def equals(self,otherCircleName):
         return otherCircleName.Value == self.Value
+
+# entity for user
+class user:
+    def __init__(self,name,id=None):
+        if id == None:
+            self.id = str(uuid.uuid4())
+            self.name = name
+        else:
+            self.id = id
+            self.name = name
--- test_circle_app.py
+++ test_circle_app.py
@@ -1,14 +1,13 @@
 import pytest
 from circle_app import circleId
 from circle_app import circleName
+from circle_app import user
 
 def test_circle_id():
     ci = circleId(123)
     assert ci.Value == 123
     ci = circleId("abc")
     assert ci.Value == "abc"
-    with pytest.raises(ValueError):
-        circleId(None)
 
 def test_circle_name():
     cn = circleName("testname")
@@ -20,3 +19,11 @@ def test_circle_name():
         circleName("a")
     with pytest.raises(ValueError):
         circleName("aaaaaaaaaaaaaaaaaaaaa")
+
+def test_user():
+    us = user(name="testUser")
+    assert us.name == "testUser"
+    assert len(us.id) == 36
+    us2 = user(name="testUser2",id="testId")
+    assert us2.name == "testUser2"
+    assert us2.id == "testId"
