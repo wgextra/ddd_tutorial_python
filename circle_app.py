5fe48606da7d3de39405243833ff5a2c5c38e055
Takumi Kumagai <kumagai.takumi1223@gmail.com>
2020-08-10 14:18:30 +0900

    implemented circleName

--- circle_app.py
+++ circle_app.py
@@ -1,5 +1,17 @@
+# value object for circle identifier
 class circleId:
     def __init__(self,value):
         if value == None:
             raise ValueError(value)
         self.Value = value
+
+# value object for cicle name
+class circleName:
+    def __init__(self,value):
+        if len(value) < 3:
+            raise ValueError("circleName must be 3 chars or longer")
+        if len(value) > 20:
+            raise ValueError("circleName must be 20 chars or shorter")
+        self.Value = value
+    def equals(self,otherCircleName):
+        return otherCircleName.Value == self.Value
--- test_circle_app.py
+++ test_circle_app.py
@@ -1,5 +1,6 @@
 import pytest
 from circle_app import circleId
+from circle_app import circleName
 
 def test_circle_id():
     ci = circleId(123)
@@ -8,3 +9,14 @@ def test_circle_id():
     assert ci.Value == "abc"
     with pytest.raises(ValueError):
         circleId(None)
+
+def test_circle_name():
+    cn = circleName("testname")
+    cn_same = circleName("testname")
+    cn_diff = circleName("testname2")
+    assert cn.equals(cn_same) == True
+    assert cn.equals(cn_diff) == False
+    with pytest.raises(ValueError):
+        circleName("a")
+    with pytest.raises(ValueError):
+        circleName("aaaaaaaaaaaaaaaaaaaaa")
